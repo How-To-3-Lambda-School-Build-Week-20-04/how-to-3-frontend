3277bf0a30978d8797f6f85e02920920dcf4f4b7
unknown <hobsond26@icloud.com>
2020-04-27 18:27:50 -0400

    created user homepage and initial landing page

--- how-to-3/src/App.js
+++ how-to-3/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+import * as yup from 'yup'
+import axios from 'axios'
 
 //Component Imports
 import Login from './components/forms/Login'
@@ -7,18 +9,74 @@ import Signup from './components/forms/Signup'
 
 //Css Import
 import './App.css';
+import UserhomePage from './components/homepages/UserhomePage';
+
+//database url
+const url = 'https://how-to-application.herokuapp.com/'
+const postNewUser= url+'/api/auth/register'
+
+
+//Yup validatiaon requirements
+const formSchema = yup.object().shape({
+  username:yup
+  .string()
+  .required('Create a new user name ')
+
+})
+
+const initialSignup = {
+  username:'',
+  email:'',
+  password:'',
+
+}
 
 function App() {
+  const [signupData,setSignupData] =useState(initialSignup)
+  const [loginData,setLoginData] = useState(null)
+
+  const onChangeHandle = evt =>{
+    const name = evt.target.name
+    const value = evt.target.value
+    setSignupData({...signupData,
+      [name] :value
+      
+      })
+    
+  }
+  const onSignup = evt =>{
+    evt.preventDefault()
+    axios.post(postNewUser,signupData)
+    .then(res=>{
+      console.log(res.data + 'success')
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+
+    setSignupData(initialSignup)
+
+  }
+
+
   return (
     <Router>
       <div className="App">
 
         <Switch>
+          <Route path='/:user/homepage'>
+            <UserhomePage
+            
+            
+            />
+          </Route>
           
           <Route path='/signup'>
           <Signup
           //Signup Props
-          
+          onChangeHandle= {onChangeHandle}
+          onSignup = {onSignup}
+          signupData ={signupData}
           />
           </Route>
           
@@ -29,7 +87,14 @@ function App() {
             
             />
           </Route>
-          
+
+          <Route path='/'>
+            <Landing
+            
+            />
+
+          </Route>
+
         </Switch>
         
       </div>
--- how-to-3/src/components/forms/Signup.js
+++ how-to-3/src/components/forms/Signup.js
@@ -4,16 +4,39 @@ import * as yup from 'yup'
 
 
 
+
+
 const Form = styled.form `
 
 `   
 
-export default function Signup() {
+export default function Signup(props) {
+    const {onChangeHandle,onSignup,signupData} = props
+    
     return (
         <Form>
-            <label htmlFor='username'>
-                UserName
-            </label>
+            <div className='input-box'>
+                <label htmlFor='username'>
+                    UserName
+                </label>
+                <input name='username' value={signupData.username} type='text' placeholder='User Namae' onChange={onChangeHandle} ></input>
+            </div>
+
+            <div className='input-box'>
+                <label htmlFor='Email'>
+                    Email 
+                </label>
+                <input name='email' type='email' value={signupData.email} placeholder='Email'onChange={onChangeHandle}></input>
+            </div>
+
+            <div className='input-box'>
+                <label htmlFor='password'>
+                    Password
+                </label>
+                <input name='password' type='password' value={signupData.password} placeholder='Password'onChange={onChangeHandle}></input>
+            </div>
+
+            <button type='submit'onClick={onSignup}>Sign Up</button>
             
         </Form>
     )
