bcce08efe4cc1b5058a4c0a0418840a3b96bc60b
MatthewSchuette <mjschuette4@gmail.com>
2020-04-28 13:08:19 -0500

    built basic register/login

--- how-to-3/src/App.js
+++ how-to-3/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Register from './components/Register';
+import PrivateRoute from './utilities/PrivateRoute';
+import HowToPage from './components/HowToPage';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App-header">
+        <Route exact path="/" component={Register} />
+          <PrivateRoute exact path="/howtopage" component={HowToPage} />
+      </div>
+    </Router>
   );
 }
 
